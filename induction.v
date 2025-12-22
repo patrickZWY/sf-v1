4cd1ec14983de17f4e8a2417a7cadef1996ccf48
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2025-12-22 13:36:14 +0800

    work on induction

--- induction.v
+++ induction.v
@@ -159,8 +159,90 @@ Proof.
     rewrite -> mult_n_1.
     reflexivity.
 Qed.
-    
 
+Theorem leb_refl : forall n:nat,
+(n <=? n) = true.
+Proof.
+    intros n.
+    induction n as [| n' IHn'].
+    simpl. reflexivity.
+    simpl.
+    rewrite -> IHn'.
+    reflexivity.
+Qed.
+
+Theorem zero_neqb_S : forall n:nat,
+0 =? (S n) = false.
+Proof.
+    intros n.
+    induction n as [|n' IHn'].
+    simpl. reflexivity.
+    simpl. reflexivity.
+Qed.
+
+Theorem andb_false_r : forall b : bool,
+andb b false = false.
+Proof.
+    intros b.
+    destruct b.
+    reflexivity.
+    reflexivity.
+Qed.
+
+Theorem S_neqb_0 : forall n:nat,
+(S n) =? 0 = false.
+Proof.
+    intros n.
+    reflexivity.
+Qed.
+
+Theorem mult_1_1 : forall n:nat, 1 * n = n.
+Proof.
+    intros n.
+    induction n as [| n' IHn'].
+    reflexivity.
+    rewrite <- mul_comm.
+    rewrite <- mult_n_1.
+    reflexivity.
+Qed.
+
+Theorem all3_spec : forall b c : bool,
+orb (andb b c) 
+    (orb (negb b) 
+        (negb c)) = true.
+Proof.
+    intros b c.
+    destruct b.
+    simpl.
+    destruct c.
+    simpl. reflexivity.
+    simpl. reflexivity.
+    simpl. reflexivity.
+Qed.
+
+Theorem mult_plus_distr_r : forall n m p : nat,
+(n + m) * p = (n * p) + (m * p).
+Proof.
+    intros n m p.
+    rewrite -> mul_comm.
+    rewrite -> mul_dist.
+    rewrite -> mul_comm.
+    replace (p * m) with (m * p).
+    reflexivity.
+    rewrite -> mul_comm.
+    reflexivity.
+Qed.
+
+Theorem mult_assoc : forall n m p : nat,
+    n * (m * p) = (n * m) * p.
+Proof.
+    intros n m p.
+    induction n as [| n' IHn'].
+    reflexivity.
+    simpl.
+    rewrite -> IHn'.
+    rewrite <- mult_plus_distr_r.
+reflexivity. Qed.
 
 
 
